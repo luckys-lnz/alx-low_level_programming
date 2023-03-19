d80f858c73846cbf455b92b79ea537e03cf9bece
luckys-lnz <lalucky22@gmail.com>
2023-03-19 14:07:49 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - A program to print the alphabets in uppercase and then lowercase
+ *       A-Z and a-z
+ *Return:0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'A'; ch <= 'Z' ch++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch <= 'z' ch++)
+{
+putchar(ch);
+}
+return (0);
+}
