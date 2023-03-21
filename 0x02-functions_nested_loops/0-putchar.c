49d9bc2236a9bf3952ff429495eb108e48645988
luckys-lnz <lalucky22@gmail.com>
2023-03-21 15:37:09 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,12 @@
 #include "main.h"
+
 /**
-  * main - execution starts here
-  *
-  * Return: 0
-  */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+    print_alphabet();
+    return (0);
 }
