06d88cd6731a534a74cbbb5efb976c18f0e2b322
luckys-lnz <lalucky22@gmail.com>
2023-04-06 10:55:49 -0700

    0.She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- *main - function that prints a string followed by a new line
- *
- *Return: Always 0
-*/
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+
 void _puts_recursion(char *s)
 {
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
 }
