62542c733a3cb698c43cffc81790289339cde382
luckys-lnz <lalucky22@gmail.com>
2023-05-01 06:39:41 -0700

    9.insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
- *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
- *
- * Return: NULL if anything fails or the address of the new node
-*/
+  * insert_nodeint_at_index - ...
+  * @head: ...
+  * @idx: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *n_node, *new;
-	unsigned int index;
+	listint_t *n_node, *iter;
+	unsigned int add_count = 1;
 
-	new = *head; /*place first node at new*/
-	n_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || n_node == NULL)
-		return (NULL);
-	n_node->n = n;
-	/* add our element to the new node*/
-	for (index = 0; head != NULL && index < idx - 1; index++)
+	if (head)
 	{
-		new = new->next;
-		if (new == NULL)
+		n_node = malloc(sizeof(listint_t));
+		if (n_node == NULL)
 			return (NULL);
-	}
-	if (idx == 0) /*if the index for new node is 0*/
-	{
-		n_node->next = *head;
-		/*The new node will be placed as node 1*/
-		*head = n_node;
-	}
-	else if (new->next)
-	{
-		n_node->next = new->next;
-		new->next = n_node;
-	}
-	else /*if node position is not in the list*/
-	{
-		n_node->next = NULL;
-		/*set next address location as NULL, indicates last node*/
-		new->next = n_node;
-	}
 
-	return (n_node);
+		n_node->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (add_count == idx)
+				{
+					n_node->next = iter->next;
+					iter->next = n_node;
+					return (n_node);
+				}
+				iter = iter->next;
+				add_count++;
+			}
+			if (idx > add_count)
+				return (NULL);
+		}
+		else
+		{
+			n_node->next = *head;
+			*head = n_node;
+		}
+		return (n_node);
+	}
+	return (NULL);
 }
