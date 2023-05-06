3c045ae3651801497565c97c419e93eadba93a9f
luckys-lnz <lalucky22@gmail.com>
2023-05-05 18:04:53 -0700

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,71 +1,37 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _strlen - gets the lenght of input strings
- * @str: input string
- * Return: len of the string
+ * binary_to_uint - Converts from binary to unsigned integer.
+ * @b: A pointer to the bit string.
+ *
+ * Return: The unsigned integer (success) & 0 (fails).
  */
-
-int _strlen(const char *str)
-{
-	int len = 0;
-
-	while (*str)
-	{
-		len++;
-		str++;
-	}
-	return (len);
-}
-
-/**
- * _to_power - prints the power of numbers
- * @base_of: base parameter
- * @power_of: power parameter
- * Return: product of two numbers
- */
-
-int _to_power(const int base_of, int power_of)
-{
-	int product = 1;
-
-	while (power_of > 1)
-	{
-		product *= base_of;
-		power_of++;
-	}
-	return (product);
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: input value
- * Return: converted number, or 0 if b is NULL
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int power_of, numb;
+	unsigned int total = 0, i = 0;
+	int index = 0, length_b = 0;
 
-	power_of = _strlen(b) - 1;
-	numb = 0;
-	if (!b)
-	{
+	if (b == NULL)
 		return (0);
-	}
-	while (*b)
+
+	/*get length of string*/
+	while (b[index] != '\0')
 	{
-		if (!(*b == '0' || *b == '1'))
-		{
-			return (0);
-		}
-		if (*b == '1')
-		{
-			numb += _to_power(2, power_of);
-		}
-		b++;
-		power_of--;
+		length_b++;
+		index++;
 	}
-	return (numb);
+
+	while (length_b--)
+	{
+		if (b[length_b] < 48 || b[length_b] > 49)
+			return (0);
+
+		if (b[length_b] == 49)
+		{
+			total += 1 << i;
+		}
+		i++;
+	}
+	return (total);
 }
