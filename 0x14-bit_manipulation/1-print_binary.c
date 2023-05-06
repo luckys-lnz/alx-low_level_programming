8ed21270cf147bf49e1c41b1fcae6d2c05f07034
luckys-lnz <lalucky22@gmail.com>
2023-05-05 17:15:10 -0700

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,15 +11,15 @@
  *
  * @n: value of n
  * @length: length of n
- * @bit_m: bit_m of n
+ * @mask: mask of n
  *
  * Return: nothing
 */
 void debug_1(unsigned long int n, int length, unsigned long int mask)
 {
 	printf("Length of %lu (n) is %i ", n, (length + 1));
-	printf("and bit_m (original value 1) ");
-	printf("is %lu based ", bit_m);
+	printf("and mask (original value 1) ");
+	printf("is %lu based ", mask);
 	printf("on [length of n - 1] == %i.\n\n", length);
 }
 
@@ -28,27 +28,27 @@ void debug_1(unsigned long int n, int length, unsigned long int mask)
  * debug_2 - print debug statements
  *
  * @n: value of n
- * @bit_m: bit_m of n
+ * @mask: mask of n
  *
  * Return: nothing
 */
-void debug_2(unsigned long int n, unsigned long int bit_m)
+void debug_2(unsigned long int n, unsigned long int mask)
 {
 	printf("Value of n is %lu, ", n);
-	printf("value of bit_m is %lu and ", bit_m);
-	printf("value of [n & bit_m] is %lu.\n\n", (n & bit_m));
+	printf("value of mask is %lu and ", mask);
+	printf("value of [n & mask] is %lu.\n\n", (n & mask));
 }
 
 /**
  * debug_3 - print debug statements
  *
- * @bit_m: bit_m of value n
+ * @mask: mask of value n
  *
  * Return: nothing
 */
-void debug_3(unsigned long int bit_m)
+void debug_3(unsigned long int mask)
 {
-	printf("\nValue of bit_m is %lu after right shifting by one.\n\n", bit_m);
+	printf("\nValue of mask is %lu after right shifting by one.\n\n", mask);
 }
 
 /**
@@ -91,31 +91,31 @@ int _length(unsigned long int n)
 void print_binary(unsigned long int n)
 {
 	int length;
-	unsigned long int bit_m = 1;
+	unsigned long int mask = 1;
 
 	length = _length(n);
 
-	if (length > 0) /*create bit_m based on length of number*/
-		bit_m <<= length; /*shift bit_m to the left by length*/
+	if (length > 0) /*create mask based on length of number*/
+		mask <<= length; /*shift mask to the left by length*/
 	#ifdef DEBUG
-	debug_1(n, length, bit_m);
+	debug_1(n, length, mask);
 	#endif
 
-	while (bit_m > 0)
+	while (mask > 0)
 	{
 		#ifdef DEBUG
-		debug_2(n, bit_m);
+		debug_2(n, mask);
 		#endif
 
-		if (n & bit_m) /*if n & bit_m == 1 print 1*/
+		if (n & mask) /*if n & mask == 1 print 1*/
 			_putchar('1');
-		else /*else if n & bit_m == 0 print 0*/
+		else /*else if n & mask == 0 print 0*/
 			_putchar('0');
 
-		bit_m >>= 1; /*shift bit_m to the right by 1*/
+		mask >>= 1; /*shift mask to the right by 1*/
 
 		#ifdef DEBUG
-		debug_3(bit_m);
+		debug_3(mask);
 		#endif
 	}
 }
