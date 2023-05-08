a99e6322548e8a89b02e37ca8787d6f754877f69
luckys-lnz <lalucky22@gmail.com>
2023-05-08 15:37:46 -0700

    1. under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,39 @@
+1)
 #include "main.h"
 
 /**
- * create_file - a function that creates a file
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * @filename: name of file to create
- * @text_content: string to write to file
- *
- * Return: 1 on success OR -1 on faliure
-*/
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int f, w_status, l = 0;
+	int f;
+	int wrd;
+	int l;
 
-	if (filename == NULL) /*check if filename is present*/
+	if (!filename)
 		return (-1);
-	/*open file by creating it and if it exists write but truncate to 0*/
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (f == -1) /*check if file creation was a success*/
+
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (f == -1)
 		return (-1);
-	if (text_content) /*write content to file if its not NULL*/
-	{
-		while (text_content[l] != '\0') /*find number of words*/
-			l++;
-		/*write to file*/
-		w_status = write(file, text_content, words);
-		if (w_status == -1) /*check if write was a success*/
+
+	if (!text_content)
+		text_content = "";
+
+	for (wrd = 0; text_content[wrd]; wrd++)
+		;
+
+	l = write(f, text_content, wrd);
+
+	if (l == -1)
 		return (-1);
-	}
-		close(f); /*close file*/
-		return (1);
+
+	close(f);
+
+	return (1);
 }
