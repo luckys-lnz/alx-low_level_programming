9131cd3f12a445a1b5a42f4e54953c47fe073eec
luckys-lnx <lalucky22@gmail.com>
2023-07-07 15:39:57 -0700

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,35 @@
 #include "main.h"
-
+/**
+ * print_binary - Converts from binary to unsigned integer.
+ * @n: A pointer to the bit string.
+ *
+ * Return: The unsigned integer (success) & 0 (fails).
+ */
 void print_binary(unsigned long int n)
 {
-  if (n == 0)
-  {
-    _putchar('0');
-    return;
-  }
 
-  unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-  int leading_zeros = 1;
+unsigned long int mask;
+int leading_zeros;
 
-  while (mask > 0)
-  {
-    if ((n & mask) != 0)
-    {
-      leading_zeros = 0;
-      _putchar('1');
-    }
-    else if (!leading_zeros)
-    {
-      _putchar('0');
-    }
-    mask >>= 1;
-  }
+if (n == 0)
+{
+_putchar('0');
+return;
 }
+mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+leading_zeros = 1;
+while (mask > 0)
+{
+if ((n & mask) != 0)
+{
+leading_zeros = 0;
+_putchar('1');
+}
+else if (!leading_zeros)
+{
+_putchar('0');
+}
+mask >>= 1;
+}
+}
+
