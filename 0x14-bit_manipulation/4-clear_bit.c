ea3ce53bfa235bf1d92d534d8b75c6913b94caff
luckys-lnx <lalucky22@gmail.com>
2023-07-08 05:06:28 -0700

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 unsigned long int mask;
 if (index >= sizeof(unsigned long int) * 8) /*converts byte to bit*/
 return (-1); /*error out of range*/
-/* Create a mask with the bit at the given index set to 0 and all other bits set to 1 */
+/* Create a mask bit at the given index set to 0 and all other bits set to 1 */
 mask = ~(1UL << index);
 /* Clear the bit at the specified index in the value pointed to by 'n' */
 *n &= mask;
