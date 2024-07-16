572c3a0c07b2c11d8ad69b588aaaa1832f05d85a
luckys-lnz <lalucky22@gmail.com>
2024-07-16 05:05:21 -0700

    linear search operation

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,18 +8,18 @@
  * @value: Value to search for
  *
  * Return: The first index where value is located,
- *          or -1 if value is not present or array is NULL
+ *	or -1 if value is not present or array is NULL
  */
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
 	if (array == NULL)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked [%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return ((int)i);
 	}
