9c39628dce3ed64c2a58edcfdf7f1d53bb1a63e5
luckys-lnz <lalucky22@gmail.com>
2024-07-17 06:24:17 -0700

    jump search, one error

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,43 +2,44 @@
 
 /**
  * jump_search - Searches for a value in a sorted array
- *		of integers using the Jump search algorithm
+ *               of integers using the Jump search algorithm.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
  *
- * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in array
- * @value: is the value to search for
- *
- * Return: index where value is located, else -1
+ * Return: Index where value is located, else -1.
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	/* Block size to jump */
-	size_t step = sqrt(size);
-	size_t prev = 0;
-	size_t start;
-	size_t  end;
-	size_t i;
+	size_t step, prev, start, end, i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
+	step = (size_t)sqrt(size);
+	prev = 0;
+
+	/* Finding the block where value is present */
 	while (prev < size && array[prev] < value)
 	{
-		/* finding the block where value is present */
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		prev += step;
 	}
-	start = (prev >=  step) ? prev - step : 0;
-	end = (prev < size) ? prev : size;
-	printf("Value found between indexes [%lu] and [%lu]\n", start, end - 1);
 
+	/* Adjust the start and end of the block */
+	start = prev - step;
+	end = (prev < size) ? prev : size;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+
+	/* Linear search within the block */
 	for (i = start; i < end; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (i);
+			return ((int)i);
 	}
+
 	return (-1);
 }
 
